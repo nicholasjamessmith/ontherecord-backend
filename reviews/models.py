8723ea953591e560d3b438a630aa4755a258c181
nicholasjamessmith <njsmith6@gmail.com>
2023-09-25 17:39:11 -0400

    CharField max_length set to 255

--- reviews/models.py
+++ reviews/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Review(models.Model):
-  image = models.CharField(max_length=1000)
-  title_of_work = models.CharField(max_length=1000)
-  artist = models.CharField(max_length=1000)
-  review = models.CharField(max_length=1000)
+  image = models.CharField(max_length=255)
+  title_of_work = models.CharField(max_length=255)
+  artist = models.CharField(max_length=255)
+  review = models.CharField(max_length=255)
 
 
