b4f6d48d72191d98a465b379e6da9623f189178a
nicholasjamessmith <njsmith6@gmail.com>
2023-09-25 17:42:18 -0400

    CharField set back to 100

--- reviews/models.py
+++ reviews/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Review(models.Model):
-  image = models.CharField(max_length=255)
-  title_of_work = models.CharField(max_length=255)
-  artist = models.CharField(max_length=255)
-  review = models.CharField(max_length=255)
+  image = models.CharField(max_length=100)
+  title_of_work = models.CharField(max_length=100)
+  artist = models.CharField(max_length=100)
+  review = models.CharField(max_length=100)
 
 
