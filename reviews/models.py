da16cad05a0d562b8d934b2a61f0287c8b2c3d36
nicholasjamessmith <njsmith6@gmail.com>
2023-09-25 17:31:58 -0400

    all routes working in postman, CharField max_length increased to 1000

--- reviews/models.py
+++ reviews/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Review(models.Model):
-  image = models.CharField(max_length=100)
-  title_of_work = models.CharField(max_length=100)
-  artist = models.CharField(max_length=100)
-  review = models.CharField(max_length=100)
+  image = models.CharField(max_length=1000)
+  title_of_work = models.CharField(max_length=1000)
+  artist = models.CharField(max_length=1000)
+  review = models.CharField(max_length=1000)
 
 
